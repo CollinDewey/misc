24a72cc41713a0b9d3651b62e2a3ffe8f01516b7
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2021-10-03 18:24:44 -0400

    Change dnsmasq_docker csv to tsv

--- dnsmasq_docker/build/startup.sh
+++ dnsmasq_docker/build/startup.sh
@@ -2,14 +2,14 @@
 
 #Verify that the executable bit is set on this script
 
-cat dhcp.csv | while IFS="	" read -r col1 col2 col3 col4
+cat dhcp.tsv | while IFS="	" read -r col1 col2 col3 col4
 do
 	if [[ $col1 != *"#"* ]] && [[ $col1 != "" ]]; then
 		echo "$col1	$col2"
 	fi
 done > arp-scan.txt
 
-cat dhcp.csv | while IFS="	" read -r col1 col2 col3 col4
+cat dhcp.tsv | while IFS="	" read -r col1 col2 col3 col4
 do
 	if [[ $col1 != *"#"* ]] && [[ $col1 != "" ]]; then
 		echo "dhcp-host=${col1:0:2}:${col1:2:2}:${col1:4:2}:${col1:6:2}:${col1:8:2}:${col1:10:2},$col2,$col3,$col4"
@@ -24,7 +24,7 @@ ff02::1 ip6-allnodes
 ff02::2 ip6-allrouters
 " > /etc/hosts
 
-cat dhcp.csv | while IFS="	" read -r col1 col2 col3 col4
+cat dhcp.tsv | while IFS="	" read -r col1 col2 col3 col4
 do
 	if [[ $col1 != *"#"* ]] && [[ $col1 != "" ]]; then
 		echo "$col3	$col2"
